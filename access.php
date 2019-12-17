6d7208f44beda88e4e461cf67c85d2f7b11dfe6d
Marco Vogt <marco.vogt@unibas.ch>
2019-12-17 09:22:46 +0100

    Minor code improvements

--- access.php
+++ access.php
@@ -30,7 +30,7 @@ class Access {
     }
 
     function register( $user, $pw ) {
-        $prep = $this->conn->prepare( "INSERT INTO dsm_user (user, password) VALUES(?,?)" );
+        $prep = $this->conn->prepare( "INSERT INTO `dsm_user` ( `user`, `password` ) VALUES(?,?)" );
         $prep->execute( array( $user, $this->hash( $pw ) ) );
         if ( $prep->errorCode() == 0 ) {
             //todo
@@ -54,7 +54,7 @@ class Access {
         if ( password_verify( $pw, $userResult[ 'password' ] ) ) {
             $unique = uniqid();
             $secret = $this->hash( $unique );
-            $insert = "INSERT INTO dsm_session ( user_id, agent, secret, last_active ) VALUES ( ?, ?, ?, NOW() )";
+            $insert = "INSERT INTO `dsm_session` ( `user_id`, `agent`, `secret`, `last_active` ) VALUES ( ?, ?, ?, NOW() )";
             $prep2 = $this->conn->prepare( $insert );
             $prep2->execute( [ $userResult[ "id" ], $this->getUserAgent(), $secret ] );
             return ( new Result() )->loginResult( $userResult[ "id" ], $userResult[ "user" ], $unique, $userResult[ "admin" ] );
@@ -94,14 +94,14 @@ class Access {
             return LoginStatus::LOGGED_OUT;
         }
         if ( $fromSpark == false ) {
-            $query = "SELECT dsm_session.secret, admin FROM dsm_user JOIN dsm_session ON dsm_user.id = dsm_session.user_id
-				WHERE dsm_session.user_id = :id AND dsm_session.agent = :agent;";
+            $query = "SELECT `dsm_session`.`secret`, `admin` FROM `dsm_user` JOIN `dsm_session` ON `dsm_user`.`id` = `dsm_session`.`user_id`
+				WHERE `dsm_session`.`user_id` = :id AND `dsm_session`.`agent` = :agent;";
         } else {
-            $query = "SELECT dsm_session.secret, admin FROM dsm_user JOIN dsm_session ON dsm_user.id = dsm_session.user_id
-				WHERE dsm_session.user_id = :id";
+            $query = "SELECT `dsm_session`.`secret`, `admin` FROM `dsm_user` JOIN `dsm_session` ON `dsm_user`.`id` = `dsm_session`.`user_id`
+				WHERE `dsm_session`.`user_id` = :id";
         }
         if ( $refresh && $fromSpark == false ) {
-            $query .= "UPDATE dsm_session SET last_active = NOW() WHERE user_id = :id AND agent = :agent;";
+            $query .= "UPDATE `dsm_session` SET `last_active` = NOW() WHERE `user_id` = :id AND `agent` = :agent;";
         }
         $prep = $this->conn->prepare( $query );
         $prep->bindParam( ":id", $id );
@@ -127,7 +127,7 @@ class Access {
     }
 
     function logout( $id ) {
-        $delete = "DELETE FROM dsm_session WHERE user_id = ? AND agent = ?";
+        $delete = "DELETE FROM `dsm_session` WHERE `user_id` = ? AND `agent` = ?";
         $prep2 = $this->conn->prepare( $delete );
         $prep2->execute( [ $id, $this->getUserAgent() ] );
     }
@@ -136,14 +136,14 @@ class Access {
      * Should be executed once a month to delete inactive sessions
      */
     function cronjob() {
-        $delete = "DELETE FROM dsm_session WHERE last_active < DATE_SUB( NOW(), INTERVAL 1 MONTH )";
+        $delete = "DELETE FROM `dsm_session` WHERE `last_active` < DATE_SUB( NOW(), INTERVAL 1 MONTH )";
         $prep = $this->conn->prepare( $delete );
         $prep->execute();
     }
 
     function changePassword( $id, $secret, $old, $new1, $new2 ) {
         if ( $this->isLoggedIn( $id, $secret ) != LoginStatus::LOGGED_OUT ) {
-            $query = "SELECT password from dsm_user WHERE id = ?";
+            $query = "SELECT `password` from `dsm_user` WHERE `id` = ?";
             $prep = $this->conn->prepare( $query );
             $prep->execute( [ $id ] );
             $user_row = $prep->fetch( PDO::FETCH_ASSOC );
@@ -151,7 +151,7 @@ class Access {
                 if ( $new1 != $new2 ) {
                     return ( new Result() )->error( "Could not change password: The new passwords do not match." );
                 } else {
-                    $query2 = "UPDATE dsm_user SET password = :pw WHERE id = :id";
+                    $query2 = "UPDATE `dsm_user` SET `password` = :pw WHERE `id` = :id";
                     $pw = $this->hash( $new1 );
                     $prep2 = $this->conn->prepare( $query2 );
                     $prep2->bindParam( ":pw", $pw );
@@ -168,7 +168,7 @@ class Access {
 
     function getUsers( $id, $secret ) {
         if ( $this->isLoggedIn( $id, $secret ) == LoginStatus::ADMIN ) {
-            $query = "SELECT id, user, email, admin FROM dsm_user";
+            $query = "SELECT `id`, `user`, `email`, `admin` FROM `dsm_user`";
             $prep = $this->conn->prepare( $query );
             $prep->execute();
             $result = $prep->fetchAll( PDO::FETCH_NUM );
@@ -181,7 +181,7 @@ class Access {
     function deleteUser( $userId, $secret, $deleteUser ) {
         // todo delete all private datasets owned by this user
         if ( $this->isLoggedIn( $userId, $secret ) == LoginStatus::ADMIN ) {
-            $query = "DELETE FROM dsm_user WHERE id = ?";
+            $query = "DELETE FROM `dsm_user` WHERE `id` = ?";
             $prep = $this->conn->prepare( $query );
             $prep->execute( [ $deleteUser ] );
             return ( new Result() )->message( "The user was deleted." );
@@ -194,7 +194,7 @@ class Access {
         if ( $this->isLoggedIn( $userId, $secret ) == LoginStatus::ADMIN ) {
             $uniqid = uniqid();
             $pw = $this->hash( $uniqid );
-            $query = "INSERT INTO dsm_user (user, admin, email, password) VALUES(:user, :admin, :email, :password)";
+            $query = "INSERT INTO `dsm_user` ( `user`, `admin`, `email`, `password` ) VALUES ( :user, :admin, :email, :password )";
             $prep = $this->conn->prepare( $query );
             $prep->bindParam( ":user", $userName );
             $prep->bindParam( ":admin", intval( (bool)$admin ) );
@@ -212,9 +212,9 @@ class Access {
         if ( $this->isLoggedIn( $adminId, $secret ) == LoginStatus::ADMIN ) {
             $updatePw = "";
             if ( $userPw != "null" ) {
-                $updatePw = ", password = :pw";
+                $updatePw = ", `password` = :pw";
             }
-            $query = "UPDATE dsm_user SET user = :name, email = :email, admin = :admin $updatePw WHERE id = :id";
+            $query = "UPDATE `dsm_user` SET `user` = :name, `email` = :email, `admin` = :admin $updatePw WHERE `id` = :id";
             $prep = $this->conn->prepare( $query );
             if ( $userPw != "null" ) {
                 $hash = $this->hash( $userPw );
@@ -240,15 +240,15 @@ class Access {
         $result = null;
         $loginStatus = $this->isLoggedIn( $id, $secret );
         if ( $loginStatus == LoginStatus::NORMAL_USER ) {
-            $query = "SELECT name, uploaded, public, id, file, owner FROM dsm_dataset WHERE public = 1 OR owner = ? ORDER BY uploaded DESC";
+            $query = "SELECT `name`, `uploaded`, `public`, `id`, `file`, `owner` FROM `dsm_dataset` WHERE `public` = 1 OR `owner` = ? ORDER BY `uploaded` DESC";
             $prep = $this->conn->prepare( $query );
             $prep->execute( [ $id ] );
             $result = $prep->fetchAll( PDO::FETCH_NUM );
         } else {
             if ( $loginStatus == LoginStatus::ADMIN ) {
-                $query = "SELECT name, uploaded, public, id, file, owner FROM dsm_dataset ORDER BY uploaded DESC";
+                $query = "SELECT `name`, `uploaded`, `public`, `id`, `file`, `owner` FROM `dsm_dataset` ORDER BY `uploaded` DESC";
             } else {
-                $query = "SELECT name, uploaded, public, id, file, owner FROM dsm_dataset WHERE public = 1 ORDER BY uploaded DESC";
+                $query = "SELECT `name`, `uploaded`, `public`, `id`, `file`, `owner` FROM `dsm_dataset` WHERE `public` = 1 ORDER BY `uploaded` DESC";
             }
             $prep = $this->conn->prepare( $query );
             $prep->execute();
@@ -261,7 +261,7 @@ class Access {
         //todo check if you have the rights to do so
         $loginStatus = $this->isLoggedIn( $userId, $secret );
         if ( $loginStatus == LoginStatus::NORMAL_USER && $userId == $dsId || $loginStatus == LoginStatus::ADMIN ) {
-            $query = "UPDATE dsm_dataset SET name = :name, public = :public WHERE id = :id";
+            $query = "UPDATE `dsm_dataset` SET `name` = :name, `public` = :public WHERE `id` = :id";
             $prep = $this->conn->prepare( $query );
             $prep->bindParam( ":id", $dsId );
             $prep->bindParam( ":name", $name );
@@ -282,7 +282,7 @@ class Access {
         $uniqueName = uniqid() . '.zip';
         $file = $this->uploadFolderPath . $uniqueName;
         if ( move_uploaded_file( $dataset[ "tmp_name" ], $file ) ) {
-            $query = "INSERT INTO dsm_dataset (name, file, public, owner, uploaded) VALUES (:name, :file, :pub, :owner, NOW())";
+            $query = "INSERT INTO `dsm_dataset` ( `name`, `file`, `public`, `owner`, `uploaded` ) VALUES ( :name, :file, :pub, :owner, NOW() )";
             $prep = $this->conn->prepare( $query );
             $prep->bindParam( ":name", $name );
             $prep->bindParam( ":file", $uniqueName );
@@ -300,7 +300,7 @@ class Access {
         if ( $loginStatus == LoginStatus::LOGGED_OUT ) {
             return ( new Result() )->error( "Please log in to delete a dataset." );
         }
-        $query = "SELECT `file`, `owner` FROM dsm_dataset WHERE id = :id";
+        $query = "SELECT `file`, `owner` FROM `dsm_dataset` WHERE `id` = :id";
         $prep = $this->conn->prepare( $query );
         $prep->bindParam( ":id", $dsId );
         $prep->execute();
@@ -312,7 +312,7 @@ class Access {
         if ( file_exists( $file ) ) {
             unlink( $file );
         }
-        $query2 = "DELETE FROM dsm_dataset WHERE id = :id";
+        $query2 = "DELETE FROM `dsm_dataset` WHERE `id` = :id";
         $prep2 = $this->conn->prepare( $query2 );
         $prep2->bindParam( ":id", $dsId );
         $prep2->execute();
